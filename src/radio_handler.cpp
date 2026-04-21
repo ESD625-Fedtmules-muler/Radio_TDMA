94f25c9f2c294351d5f555a2969f76521b7f4749
aolss23 <159021345+aolss23@users.noreply.github.com>
2026-04-21 08:56:20 +0200

    radio_handler

--- src/radio_handler.cpp
+++ src/radio_handler.cpp
@@ -6,21 +6,7 @@
 #include <ESP32-c3_pinout.h>
 #include <main.h>
 
-RF24 radio(PIN_CE, PIN_CSN);
-
-
-void block_item::print_payload(){
-  Serial.printf("Payload (hex):\n");
-  for (int i = 0; i < 32; i++) {
-      // Print byte i 2-cifret hex
-      Serial.printf("%02X ", block_payload[i]);
-      // Ny linje hver 8 bytes for readability
-      if ((i + 1) % 8 == 0) {
-          Serial.printf("\n");
-      }
-  }
-  Serial.printf("CRC: %s\n", crc ? "true" : "false");
-}
+RF24 radio(PIN_RF_CE, PIN_RF_CSN);
 
 
 bool Assert_setting(bool result, const char* msg){
@@ -35,12 +21,22 @@ bool Assert_setting(bool result, const char* msg){
 
 
 void setup_modem(rf24_pa_dbm_e power_level){
-  SPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI);
+  SPI.begin(PIN_RF_SCK, PIN_RF_MISO, PIN_RF_MOSI);
   SPI.setFrequency(8000000);
   
   Assert_setting(radio.begin(&SPI), "Setting up modem (trust this one)");
   Assert_setting(radio.isChipConnected(), "Chip connected (dont trust this one too much)");
+  if(radio.isPVariant()){
+    Serial.println("CHIP is NRF24L01+");
+  } else{
+    Serial.println("CHIP is NRF24L01");
+  }
+  radio.powerDown();
+  delay(1);
   
+  radio.powerUp();
+  delay(1);
+
   radio.disableAckPayload(); //SLår acks fra
   radio.setDataRate(network_params.bitRate); //1 MBITS / S 
   radio.setAutoAck(false); //SLår auto acks fra  i modtager
@@ -48,15 +44,13 @@ void setup_modem(rf24_pa_dbm_e power_level){
   radio.setChannel(network_params.channel); //Vi bruger channel 10
   radio.openWritingPipe(network_params.pipe_name);
   radio.openReadingPipe(0, network_params.pipe_name);
-  pinMode(PIN_SDA, OUTPUT);
-  pinMode(PIN_SCL, OUTPUT);
+
 }
 
 
 void modem_tx(){
   radio.stopListening(); //Fortæller svinet den skal gå i tx
   //TODO Sætte GPIO rf switches
-  digitalWrite(PIN_SCL, HIGH);
 }
 
 
@@ -64,5 +58,4 @@ void modem_tx(){
 void modem_rx(){
   radio.startListening();
   //TODO Sætte GPIO rf switches
-  digitalWrite(PIN_SCL, LOW);
 }
